ed9fa3e5763be77f2a000f81a7207130107b88b0
ivan_ochc <d4.johnx@gmail.com>
2015-12-15 17:02:08 +0200

    ticket:1 1. Add new tests

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,14 +1,14 @@
 from apps.hello.models import User
+from django.core.urlresolvers import reverse
 from django.db import IntegrityError
-from django.test import TestCase
-
+from django.test import TestCase, Client
 # Create your tests here.
 from wheel.signatures import assertTrue
 
 
 class ContactTests(TestCase):
     def test_create_contact(self):
-        contact = User.objects.create(username='test',email='test@email.com')
+        contact = User.objects.create(username='test', email='test@email.com')
         assert(contact.username == 'test')
         assert(contact.email == 'test@email.com')
         contact.save()
@@ -22,6 +22,22 @@ class ContactTests(TestCase):
         User.objects.filter(pk=contact_pk).delete()
         self.assertFalse(User.objects.filter(pk=contact_pk).exists())
 
+    def test_create_superuser(self):
+        super_user = User.objects.create_superuser("test@email.com", "admin", username="admin")
+        assertTrue(super_user.is_admin)
+
+    def test_user_name_is_required(self):
+        try:
+            user = User.objects.create_user("test@email.com")
+        except ValueError as e:
+            self.assertEquals(str(e), 'Users must have a valid username.')
+
+    def test_email_is_valid(self):
+        try:
+            user = User.objects.create_user("test@email", username="test")
+        except ValueError as e:
+            self.assertEquals(str(e), 'Users must have a valid email address.')
+
     def test_only_unique_emails_are_accepted(self):
         error_occurred = False
         contact = User.objects.create(email='test@email.com')
@@ -31,4 +47,12 @@ class ContactTests(TestCase):
             error_occurred = True
 
         self.assertTrue(error_occurred)
+
+    def test_home_view(self):
+        contact = User.objects.create(username='test', email='test@email.com')
+        contact.save()
+        client = Client()
+        response = client.get(reverse('home'))
+        self.assertEquals(response.status_code, 200)
+
 
