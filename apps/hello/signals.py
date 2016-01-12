f47c6a4c31f1a845d1ae26a6c5f116451fb57d88
ivan_ochc <d4.johnx@gmail.com>
2016-01-12 18:25:41 +0200

    ticket:8 signals were fixed

--- apps/hello/signals.py
+++ apps/hello/signals.py
@@ -1,24 +1,20 @@
 from apps.hello import models
-from apps.hello.models import SignalsLog
-from django.apps import apps
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 
 @receiver(post_save)
 def model_post_save(sender, **kwargs):
-    if sender in apps.get_models():
-        if sender is not SignalsLog:
-            save('Save', format(kwargs['instance'].__dict__))
-    return
+    if sender is models.SignalsLog:
+        return
+    save('Save', format(kwargs['instance'].__dict__))
 
 
 @receiver(post_delete)
 def model_post_delete(sender, **kwargs):
-    if sender in apps.get_models():
-        if sender is not SignalsLog:
-            save('Delete', format(kwargs['instance'].__dict__))
-    return
+    if sender is models.SignalsLog:
+        return
+    save('Delete', format(kwargs['instance'].__dict__))
 
 
 def save(type, payload):
