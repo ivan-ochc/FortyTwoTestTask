c2bf61472f926000fe040e8bb6317491564aa62c
ivan_ochc <d4.johnx@gmail.com>
2016-01-13 16:32:06 +0200

    master: migration was fixed

--- apps/hello/migrations/0009_webrequest_priority.py
+++ apps/hello/migrations/0009_webrequest_priority.py
@@ -7,7 +7,7 @@ from django.db import models, migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('hello', '0007_auto_20151224_1129'),
+        ('hello', '0008_signalslog'),
     ]
 
     operations = [
